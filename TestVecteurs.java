99b341dba26987987416d04d77d64120ddf30ec4
Ryam_Isaac <ryamisaac1@gmail.com>
2025-05-21 13:15:00 -0400

    Update TestVecteurs.java

--- TestVecteurs.java
+++ TestVecteurs.java
@@ -5,37 +5,48 @@ import java.util.Vector;
 
 public class TestVecteurs {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Création d'un vecteur sans générique
         Vector v = new Vector(3);
 
-        // insertion ds le vecteur en une étape
-        v.add(new Point(3,3));
+        // insertion dans le vecteur en une étape
+        v.add(new Point(3, 3));
 
-        // insertion ds le vecteur en deux étapes
-        Point p = new Point(2,4);
+        // insertion dans le vecteur en deux étapes
+        Point p = new Point(2, 4);
         v.add(p);
-        v.add (new Point(5,5));
-        v.insertElementAt(new Point(8,5), 0);
-        v.insertElementAt(new Point(4,7), 2);
+        v.add(new Point(5, 5));
+        v.insertElementAt(new Point(8, 5), 0);
+        v.insertElementAt(new Point(4, 7), 2);
         v.removeElementAt(4);
 
-        //1 afficher la capacité du vecteur
+        // Affichage du contenu
+        System.out.println("Contenu du vecteur :");
+        for (int i = 0; i < v.size(); i++) {
+            System.out.println("Élément " + i + " : " + v.get(i));
+        }
 
-        //2 afficher la grandeur du vecteur
+        // 1. Afficher la capacité du vecteur
+        System.out.println("1. Capacité du vecteur : " + v.capacity());
 
-        //3 afficher la valeur de la donnée x du premier élément du vecteur
-        // sans enlever l'élément du vecteur
+        // 2. Afficher la grandeur du vecteur
+        System.out.println("2. Taille (grandeur) du vecteur : " + v.size());
 
-        // afficher la valeur de la donnée y du deuxième élément du vecteur
-        // sans enlever l'élément du vecteur
+        // 3. Afficher la donnée x du 1er élément
+        System.out.println("3. Valeur x du 1er élément : " + ((Point) v.get(0)).x);
 
-        // afficher la valeur de la donnée x du troisième élément du vecteur
-        // sans enlever l'élément du vecteur
+        // 4. Afficher la donnée y du 2e élément
+        System.out.println("4. Valeur y du 2e élément : " + ((Point) v.get(1)).y);
 
-        // à l'aide d'une boucle, faire le total des valeurs des données x de tous les
-        // éléments du vecteur et l'afficher
-     
+        // 5. Afficher la donnée x du 3e élément
+        System.out.println("5. Valeur x du 3e élément : " + ((Point) v.get(2)).x);
+
+        // 6. Calcul du total des x
+        int totalX = 0;
+        for (int i = 0; i < v.size(); i++) {
+            Point pt = (Point) v.get(i);
+            totalX += pt.x;
+        }
+        System.out.println("6. Total des valeurs x : " + totalX);
     }
 }
